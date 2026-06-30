a098ba0d832f8cc2cc789b426002890df6c6b667
Cell <marcell.made@gmail.com>
2026-06-30 05:41:07 -0400

    added particles

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import GUI from "lil-gui";
+import { materialColor } from "three/tsl";
 
 /**
  * Debug
@@ -10,9 +11,13 @@ const parameters = {
   materialColor: "#ffeded",
 };
 
-gui.addColor(parameters, "materialColor").onChange(() => {
-  material.color.set(parameters.materialColor);
-});
+gui
+  .addColor(parameters, "materialColor")
+  .onChange(() => 
+  {
+    material.color.set(parameters.materialColor);
+    particlesMaterial.color.set(parameters.materialColor)
+  });
 
 /**
  * Base
@@ -91,6 +96,41 @@ window.addEventListener("resize", () => {
 });
 
 /**
+ * Particles
+ */
+// Geometry
+const particlesCount = 200
+const positions = new Float32Array(particlesCount * 3)
+
+for(let i = 0; i < particlesCount; i++)
+{
+  positions[i * 3 + 0] = Math.random()
+  positions[i * 3 + 1] = Math.random()
+  positions[i * 3 + 2] = Math.random()
+}
+
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+
+// Material
+const particlesMaterial = new THREE.PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03
+})
+
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+for(let i  = 0; i < particlesCount; i++)
+{
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+  positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+/**
  * Camera
  */
 // Group
@@ -115,7 +155,7 @@ window.addEventListener('scroll', () =>
   {
     scrollY = window.scrollY
 
-    console.log(scrollY)
+    //console.log(scrollY)
   })
 
 /**
@@ -152,17 +192,22 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  * Animate
  */
 const clock = new THREE.Clock();
+let previousTime = 0
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime
+  previousTime = elapsedTime
+
+  //console.log(deltaTime)
 
   // Animate camera
   camera.position.y = - scrollY / sizes.height * objectsDistance
 
-  const parallaxX = cursor.x
-  const parallaxY = - cursor.y
-  cameraGroup.position.x = parallaxX
-  cameraGroup.position.y = parallaxY
+  const parallaxX = cursor.x * 0.5
+  const parallaxY = - cursor.y * 0.5
+  cameraGroup.position.x =+ (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+  cameraGroup.position.y =+ (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
   // Animate Meshes
   for(const mesh of sectionMeshes){
